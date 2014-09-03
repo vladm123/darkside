862fee423a515c02456c8673892a3e7068114a27
Vlad Manea <vlad.c.manea@gmail.com>
2014-09-03 11:07:01 +0200

    Adding script injection

--- dark-side-debitoor.user.js
+++ dark-side-debitoor.user.js
@@ -1,9 +1,12 @@
-	var head = document.head;
-	var link = document.createElement('link');
+var head = document.head;
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://rawgit.com/vladm123/darkside/master/darkside.css';
+head.appendChild(link);
 
-	link.rel = 'stylesheet';
-	link.href = 'https://rawgit.com/vladm123/darkside/master/darkside.css';
-	
-	head.appendChild(link);
+var body = document.body;
+var script = document.createElement('script');
+script.src = 'https://rawgit.com/vladm123/darkside/master/script.js';
+body.appendChild(script);
 
-	console.log('Welcome to the dark side of e-conomic and debitoor');
+console.log('Welcome to the dark side of e-conomic and debitoor with scriptz');
