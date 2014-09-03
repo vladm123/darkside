1a0adf3a0641862643479a3b9a596678aa2d9f5d
vma <vma@e-conomic.com>
2014-09-03 20:45:32 +0200

    Adding for economic

--- darkside-e-conomic.js
+++ darkside-e-conomic.js
@@ -1,1 +1,11 @@
-console.log('Thanks BHO');
+	if (window.location.origin == 'http://bho' && typeof jQuery !== undefined) {
+		jQuery(document).ready(function() {
+			setTimeout(function() {
+				$('svg > rect').attr('fill', '#4c516b');
+				$('svg path[fill=none]').attr('stroke', '#6c718b');
+				$('svg g text').css('fill', '#acb1cb');
+			}, 300);
+			
+			console.log($('frame').length);
+		});
+	}
