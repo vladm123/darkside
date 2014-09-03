50a2432c1bd6664b138a19b9e8b32f72d0465ae0
vma <vma@e-conomic.com>
2014-09-03 12:08:43 +0200

    Adding localhost links

--- dark-side-debitoor.user.js
+++ dark-side-debitoor.user.js
@@ -1,12 +1,12 @@
 var head = document.head;
 var link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'https://vladm123.github.io/darkside/darkside.css';
+link.href = 'https://localhost:8080/darkside.css';
 head.appendChild(link);
 
 var body = document.body;
 var script = document.createElement('script');
-script.src = 'https://vladm123.github.io/darkside/script.js';
+script.src = 'https://localhost:8080/script.js';
 body.appendChild(script);
 
 console.log('Welcome to the dark side of e-conomic and debitoor with scriptz');
