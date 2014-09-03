816af6c8fa3b24029328ad17110c7e2712365478
vma <vma@e-conomic.com>
2014-09-03 11:59:51 +0200

    Adding the s in the https

--- dark-side-debitoor.user.js
+++ dark-side-debitoor.user.js
@@ -1,12 +1,12 @@
 var head = document.head;
 var link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'http://vladm123.github.io/darkside/darkside.css';
+link.href = 'https://vladm123.github.io/darkside/darkside.css';
 head.appendChild(link);
 
 var body = document.body;
 var script = document.createElement('script');
-script.src = 'http://vladm123.github.io/darkside/script.js';
+script.src = 'https://vladm123.github.io/darkside/script.js';
 body.appendChild(script);
 
 console.log('Welcome to the dark side of e-conomic and debitoor with scriptz');
