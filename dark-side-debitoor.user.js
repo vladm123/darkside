62d80d22fe188a2ed2bcab9ae60959f51b9faf74
Vlad Manea <vlad.c.manea@gmail.com>
2014-09-03 10:09:52 +0200

    Adding css inject

--- dark-side-debitoor.user.js
+++ dark-side-debitoor.user.js
@@ -1,1 +1,2 @@
-console.log('Welcome to dark side of debitoor');
+$('body').append('<link rel="stylesheet" href="https://raw.githubusercontent.com/vladm123/darkside/master/darkside.css" />');
+console.log('Welcome to the dark side of e-conomic and debitoor');
