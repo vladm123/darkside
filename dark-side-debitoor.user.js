f15fac3b7af02c86b006c76b424636419968c8f3
Vlad Manea <vlad.c.manea@gmail.com>
2014-09-03 11:42:26 +0200

    Adding new links to user js file

--- dark-side-debitoor.user.js
+++ dark-side-debitoor.user.js
@@ -1,12 +1,12 @@
 var head = document.head;
 var link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'https://rawgit.com/vladm123/darkside/master/darkside.css';
+link.href = 'http://vladm123.github.io/darkside/darkside.css';
 head.appendChild(link);
 
 var body = document.body;
 var script = document.createElement('script');
-script.src = 'https://rawgit.com/vladm123/darkside/master/script.js';
+script.src = 'http://vladm123.github.io/darkside/script.js';
 body.appendChild(script);
 
 console.log('Welcome to the dark side of e-conomic and debitoor with scriptz');
