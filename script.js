c37c64bcaa90c1fa2cbf6273d74443e01a36b980
vma <vma@e-conomic.com>
2014-09-03 14:20:25 +0200

    Adding merged versions

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 if (typeof jQuery !== undefined) {
 	jQuery(document).ready(function() {
 		console.log('JQUERY works!!!111one');
+		
 		$('body').append('<div class="theme-switcher"><ul><li class="white"><div/></li><li class="dark"><div/></li></ul></div>');
+		setInterval(function() {
+			$('svg > rect.highcharts-background').attr('fill', '#4c516b');
+			$('svg path[fill=none]').attr('stroke', '#6c718b');
+			$('svg g text').css('fill', '#acb1cb');
+		}, 100);
 	});
 }
