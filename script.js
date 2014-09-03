a6be460d885fbcdeebec49e829516997c95a47fe
Andrii Shumada <eagleeyes91@gmail.com>
2014-09-03 14:09:12 +0200

    switcher added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 if (typeof jQuery !== undefined) {
 	jQuery(document).ready(function() {
 		console.log('JQUERY works!!!111one');
+		$('body').append('<div class="theme-switcher"><ul><li class="white"><div/></li><li class="dark"><div/></li></ul></div>');
 	});
 }
