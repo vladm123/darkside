47ea1eddc1463e2dbb0c5040165d3af480b4b41e
Vlad Manea <vlad.c.manea@gmail.com>
2014-09-03 10:26:54 +0200

    Asdding inject attempt

--- dark-side-debitoor.user.js
+++ dark-side-debitoor.user.js
@@ -1,2 +1,10 @@
-$('body').append('<link rel="stylesheet" href="https://raw.githubusercontent.com/vladm123/darkside/master/darkside.css" />');
-console.log('Welcome to the dark side of e-conomic and debitoor');
+	var head = document.head;
+	var link = document.createElement('link');
+
+	link.type = 'text/css';
+	link.rel = 'stylesheet';
+	link.href = 'https://raw.githubusercontent.com/vladm123/darkside/master/darkside.css';
+	
+	head.appendChild(link);
+
+	console.log('Welcome to the dark side of e-conomic and debitoor');
